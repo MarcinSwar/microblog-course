1951b304263e63259d6dc9c2142ce20ca0eec7f9
MarcinSwar <C1nx@github>
2018-05-24 21:12:28 +0200

    [add] - Wall

--- src/main/java/pl/mswar/microblog_course/model/Wall.java
+++ src/main/java/pl/mswar/microblog_course/model/Wall.java
@@ -3,10 +3,29 @@ package pl.mswar.microblog_course.model;
 import java.util.List;
 
 public class Wall {
-    private List<UserPost>;
+    private List<UserPost> wallPosts;
 
     public Wall() {
+        // empty
     }
 
+    public Wall(List<UserPost> wallPosts) {
+        this.wallPosts = wallPosts;
+    }
+
+    public List<UserPost> getWallPosts() {
+        return wallPosts;
+    }
+
+    public void setWallPosts(List<UserPost> wallPosts) {
+        this.wallPosts = wallPosts;
+    }
+
+    @Override
+    public String toString() {
+        return "Wall{" +
+                "wallPosts=" + wallPosts +
+                '}';
+    }
 
 }
